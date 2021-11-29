05fb39075f4723f2b3b3fa0d16b1df738243578a
Rohith Satya Nivas Muchakarla <58270557+dreamhacker123@users.noreply.github.com>
2021-11-30 03:09:31 +0530

    Update counter.js

--- src/components/counter.js
+++ src/components/counter.js
@@ -33,13 +33,13 @@ const Counter = ()=>{
     return (
         <>
         <div className = "load">{loading?
-        <div><div class="sk-chase">
-        <div class="sk-chase-dot"></div>
-        <div class="sk-chase-dot"></div>
-        <div class="sk-chase-dot"></div>
-        <div class="sk-chase-dot"></div>
-        <div class="sk-chase-dot"></div>
-        <div class="sk-chase-dot"></div>
+        <div><div className="sk-chase">
+        <div className="sk-chase-dot"></div>
+        <div className="sk-chase-dot"></div>
+        <div className="sk-chase-dot"></div>
+        <div className="sk-chase-dot"></div>
+        <div className="sk-chase-dot"></div>
+        <div className="sk-chase-dot"></div>
         </div><p className="csaved">Saving counter values...</p></div>:<div className="csaved">Value saved ✔️</div>}</div>
         <div className="cnter">
         <button className="btnl" onClick={()=>setCount(count-1)}>-</button>
